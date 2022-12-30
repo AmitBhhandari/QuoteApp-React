d09a5bb65e7da76075f3663ef1ab8a84dad82d67
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2022-12-30 21:37:05 +0530

    optimized  code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+
+import React,{Suspense} from "react";
+
 import { Route, Switch, Redirect } from "react-router-dom";
-import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import QuoteDetails from "./pages/QuoteDetails";
+//import AllQuotes from "./pages/AllQuotes";
+//import NewQuote from "./pages/NewQuote";
+//import QuoteDetails from "./pages/QuoteDetails";
 import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+//import NotFound from "./pages/NotFound";
+
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetails = React.lazy(() => import("./pages/QuoteDetails"));
+const NotFound = React.lazy(()=> import("./pages/NotFound"))
+const AllQuotes = React.lazy(()=> import("./pages/AllQuotes")); 
+
 
 function App() {
   return (
     <Layout>
+      <Suspense fallback={<div className="centered"><LoadingSpinner/></div>}>
       <Switch>
         <Route path="/" exact>
           <Redirect to="/quotes" />
@@ -29,6 +41,7 @@ function App() {
           <NotFound />
         </Route>
       </Switch>
+      </Suspense>
     </Layout>
   );
 }
